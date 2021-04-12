90d1e0bd4182f38dbfa52f9bf6286a54589e53ff
Samantha <Sambollano54@gmail.com>
2021-04-11 22:52:15 -0500

    .

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    generateEl.addEventListener('click', () => {
+    const length = lengthEl.value;
+
+    })
+    passwordText.value = password;
+  
+  }
+  myInput.onblur = function() {
+    document.getElementById("message").style.display = "none";
+  }
+  myInput.onkeyup = function() {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if(myInput.value.match(lowerCaseLetters)) {
+      letter.classList.remove("invalid");
+      letter.classList.add("valid");
+    } else {
+      letter.classList.remove("valid");
+      letter.classList.add("invalid");
+  }
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+  var myInput = document.getElementById("psw");
+  var letter = document.getElementById("letter");
+  var capital = document.getElementById("capital");
+  var number = document.getElementById("number");
+  var length = document.getElementById("length");  
+}
+  var length = 8,
+  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+  retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+  retVal += charset.charAt(Math.floor(Math.random() * n)); }
